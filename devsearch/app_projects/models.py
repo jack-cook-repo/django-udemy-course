1d4694ce771e3edcb479efbd8ff8b71f280528a8
jack-cook-repo <61659989+jack-cook-repo@users.noreply.github.com>
2021-09-19 18:54:04 +0100

    X

--- devsearch/app_projects/models.py
+++ devsearch/app_projects/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
 # Create your models here.
+# A Django 'Model' can be considered as a table, and is built with Python classes
--- devsearch/app_projects/views.py
+++ devsearch/app_projects/views.py
@@ -4,9 +4,32 @@ from django.http import HttpResponse
 # Create your views here.
 HTML_TEMPLATE_FOLDER = 'app_projects/'
 
+# Dict of projects to display
+list_projects = [
+    {'id': '1',
+     'title': 'Ecommerce site',
+     'description': 'Fully functional ecommerce site'
+     },
+    {'id': '2',
+     'title': 'Portfolio site',
+     'description': 'Built out portfolio website'
+     },
+    {'id': '3',
+     'title': 'Social networking site',
+     'description': 'Test social networking site'
+     }
+]
+
 
 def get_projects(request):
-    return render(request, template_name=HTML_TEMPLATE_FOLDER+'projects.html')
+    page = 'projects'
+    number = 9
+
+    return render(request,
+                  template_name=HTML_TEMPLATE_FOLDER+'projects.html',
+                  context={'page': page,
+                           'number': number,
+                           'projects': list_projects})
 
 
 def get_project(request, pk):
@@ -15,4 +38,10 @@ def get_project(request, pk):
 
     Note, the name 'pk' must match the urlpatterns argument name exactly - i.e. 'project/<str:pk>'
     """
-    return render(request, template_name=HTML_TEMPLATE_FOLDER+'single-project.html')
+    project_object = None
+    for project in list_projects:
+        if project['id'] == pk:
+            project_object = project
+    return render(request,
+                  template_name=HTML_TEMPLATE_FOLDER+'single-project.html',
+                  context={'project': project_object})
